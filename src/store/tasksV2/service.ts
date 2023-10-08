2498f26daa8473b3d194afa75f863296623476ac
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2023-10-08 17:16:19 +0200

    JSON to read data

--- src/store/tasksV2/service.ts
+++ src/store/tasksV2/service.ts
@@ -1,40 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { ITask } from 'src/app/task/task-type';
+import data from 'src/db/tasksV2.json'
 
 @Injectable({
   providedIn: 'root',
 })
 
 export class TaskServiceV2 {
-  private tasksV2: ITask[] = [
-    {
-      id: 1,
-      name: 'Task1',
-      description: 'toto',
-      status: 'Todo',
-      enabled: true,
-    },
-    {
-      id: 2,
-      name: 'Task2',
-      description: 'tutu',
-      status: 'WIP',
-      enabled: true,
-    },
-    {
-      id: 3,
-      name: 'Task3',
-      description: 'titi',
-      status: 'Done',
-      enabled: true,
-    },
-  ];
 
   constructor() {}
 
   getInitialTasks(): Observable<ITask[]> {
     // Simulate fetching tasks from a data source (e.g., an API)
-    return of(this.tasksV2);
+    return of(data.tasksV2 as ITask[]);
   }
 }
